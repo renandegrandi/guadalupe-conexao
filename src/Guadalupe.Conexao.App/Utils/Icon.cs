d89bd26c0331e54b5aadb5b66020fee30f8df602
Renan Degrandi <renan.degrandi@sitemercado.com.br>
2020-10-18 17:20:27 -0300

    - Removendo arquivos que não estãosendo utilizados.

--- src/Guadalupe.Conexao.App/Utils/Icon.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Guadalupe.Conexao.App.Utils
-{
-    public static class Icon
-    {
-        public const string FacebookOfficial = "\uF230";
-        public const string Home = "\uE800";
-        public const string Profile = "\uE801";
-    }
-}
