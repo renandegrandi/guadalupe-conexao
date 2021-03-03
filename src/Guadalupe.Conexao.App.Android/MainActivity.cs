0e6b6e1119ac88502b86463d576280ea877c9e1c
Tiago Ladeia <dtiagorl@gmail.com>
2021-03-03 01:26:20 -0300

    Fix Icon Android 10

--- src/Guadalupe.Conexao.App.Android/MainActivity.cs
+++ src/Guadalupe.Conexao.App.Android/MainActivity.cs
@@ -13,7 +13,7 @@ using System.Linq;
 
 namespace Guadalupe.Conexao.App.Droid
 {
-    [Activity(Label = "Conexão", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Conexão", Icon = "@mipmap/icon", RoundIcon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
