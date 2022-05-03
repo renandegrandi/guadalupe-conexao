86714076a0bf64836965787bbfb070f2d4ab3dce
Tiago Ladeia <dtiagorl@gmail.com>
2022-05-03 00:27:59 -0300

    fix: changing api loclhost link and adding vscode files

--- src/Guadalupe.Conexao.App/Configuration.cs
+++ src/Guadalupe.Conexao.App/Configuration.cs
@@ -2,7 +2,7 @@
 {
     public static class Configuration
     {
-        public const string ConexaoApi = "http://192.168.1.114:4000/api/";
+        public const string ConexaoApi = "http://192.168.0.43:4000/api/";
         public const string Assets = "https://guadalupestorage.blob.core.windows.net/assets/";
     }
 }
